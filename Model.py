91ea9c974db2c3c5c0b350490c9c8e74fc41d772
subashcs <subashsapkota59@gmail.com>
2019-08-10 16:36:41 +0545

    Add files via upload
    API files added

---
+++ Model.py
@@ -0,0 +1,66 @@
+from flask import Flask ,jsonify , request,json
+import pickle
+from nltk.corpus import stopwords
+
+import pandas as pd
+import numpy as np
+
+import nltk
+
+import re
+REPLACE_BY_SPACE_RE = re.compile('[/(){}\[\]\|@,;]')
+BAD_SYMBOLS_RE = re.compile('[^0-9a-z #+_]')
+
+STOPWORDS = set(stopwords.words('english'))
+
+#prepare text clean function
+
+def clean_text(text):
+    """
+        text: a string
+        
+        return: modified initial string
+    """
+   ## we dont need text = BeautifulSoup(text, "lxml").text # HTML decoding
+    text = text.lower() # lowercase text
+    text = REPLACE_BY_SPACE_RE.sub(' ', text) # replace REPLACE_BY_SPACE_RE symbols by space in text
+    text = BAD_SYMBOLS_RE.sub('', text) # delete symbols which are in BAD_SYMBOLS_RE from text
+    text = ' '.join(word for word in text.split() if word not in STOPWORDS) # delete stopwors from text
+    return text
+    
+
+app = Flask(__name__)
+
+if __name__ == '__main__':
+    app.run(debug=True,host='0.0.0.0')
+
+filename = 'finalized_model.sav'
+loaded_model = pickle.load(open(filename, 'rb'))
+
+@app.route('/')
+def hello_world():
+    return undefined
+
+@app.route('/<content>')
+def show_user_profile(content):
+    # show the user profile for that user
+    str1 = ''.join(str(e) for e in content)
+    ToPredict=clean_text(content)
+    result = loaded_model.predict([ToPredict])
+    res = ''.join(str(e) for e in result)
+    return jsonify({"category":res})
+
+
+@app.route('/classify',methods = ['POST'])
+def classify():
+    data = request.get_json()
+    content = data['content']
+    str1 = ''.join(str(e) for e in content)
+    ToPredict=clean_text(content)
+    result = loaded_model.predict([ToPredict])
+    res = ''.join(str(e) for e in result)
+    return jsonify({"category":res})
+
+@app.route('/test')
+def testData():
+    return jsonify({"result":"success"})    
